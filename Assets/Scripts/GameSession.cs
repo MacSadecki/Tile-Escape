a60eeced423701babebce97cf976ca59c25227f8
MacSadecki <mac.sadecki@gmail.com>
2023-10-10 15:10:21 +0200

    Initial commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+    [SerializeField] int playerScore = 0;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    void Start() 
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if (playerLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    public void ScoreAddition(int pointsToAdd)
+    {
+        playerScore += pointsToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    void ResetGameSession()
+    {   
+        FindObjectOfType<ScenePersist>().ResetScenePersist(); 
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+
+    void TakeLife()
+    {   
+        playerLives--;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        livesText.text = playerLives.ToString();
+    }
+}
